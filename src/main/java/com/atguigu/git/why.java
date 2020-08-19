8a55207fae87949c7b70fb8c769c529a71c79561
why <523249046@qq.com>
2020-08-19 09:20:41 +0800

    xinwenjian

---
+++ src/main/java/com/atguigu/git/why.java
@@ -0,0 +1,9 @@
+package com.atguigu.git;
+
+public class why {
+    public static void main(String[] args) {
+        System.out.println("good");
+        System.out.println("good");
+        System.out.println("study");
+    }
+}
